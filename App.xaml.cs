baa66248dba76cd8f0fc799aa1c1170d3f58460c
Malikova1.1 <ailina.malikova@yandex.ru>
2025-10-20 13:32:09 +0500

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab1._2MalikovaAA_BPI2301
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
