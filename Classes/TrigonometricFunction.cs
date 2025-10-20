497004604065dd4768e4e25084804d328f422446
Malikova1.1 <ailina.malikova@yandex.ru>
2025-10-20 17:42:35 +0500

    интерфейс и базовый класс

---
+++ Classes/TrigonometricFunction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._2MalikovaAA_BPI2301.Classes
+{
+    class Class1
+    {
+    }
+}
